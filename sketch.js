4e41293ba8389d48f6beb753fd4c8245e74a172b
Aryanprabhakar6628 <83065010+Aryanprabhakar6628@users.noreply.github.com>
2021-06-19 02:52:56 -0700

    Update sketch.js
    correcting the file name fairyImage on the line number 15 .

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function preload()
 	starImg = loadImage("images/star.png");
 	bgImg = loadImage("images/starNight.png");
 	//load animation for fairy here
-	fairyImage = loadAnimation("images/fairyimage1.png","images/fairyimage2.png");
+	fairyImage = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
 	fairySound = loadSound("sound/JoyMusic.mp3");
 }
 
